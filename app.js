479a81167d87d5be1d98d9262b7767cb86867c32
Drápela František <L20220035@SSPTAJI.CZ>
2024-09-30 12:24:35 +0200

    rfubzhbuoghzurf

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ if ('serviceWorker' in navigator) {
     });
 }
  
-function sayHello() {
-    document.getElementById('hello').innerText = 'Hello world';
+function appendToDisplay(value) {
+    const display = document.getElementById("display");
+    if (display.value === "0") {
+        display.value = value;
+    } else {
+        display.value += value;
+    }
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "0";
+}
+
+function calculateResult() {
+    const display = document.getElementById("display");"Pája má malej pipik";
 }
